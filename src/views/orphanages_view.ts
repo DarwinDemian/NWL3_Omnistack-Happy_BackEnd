b755cd44278c8bcde8875ca35342726e5089f516
Demian6662 <darwindemian@hotmail.com>
2020-10-16 04:52:41 -0300

    adding files

---
+++ src/views/orphanages_view.ts
@@ -0,0 +1,22 @@
+import OrphanageTS from '../models/Orphanage';
+import imagesView from './images_view';
+
+export default {
+  render(orphanage: OrphanageTS) {
+    return {
+      "id": orphanage.id,
+      "name": orphanage.name,
+      "latitude": orphanage.latitude,
+      "longitude": orphanage.longitude,
+      "about": orphanage.about,
+      "instructions": orphanage.instructions,
+      "opening_hours": orphanage.opening_hours,
+      "open_on_weekends": orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images),
+    };
+  },
+
+  renderMany(orphanages: OrphanageTS[]) {
+    return orphanages.map(orphanage => this.render(orphanage));
+  }
+};
